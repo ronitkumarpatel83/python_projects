ce72f2f522159f3f7abaf2ec7ea15337bd1ca183
Ronit Kumar Patel <ronitkumarpatel83@gmail.com>
2025-12-13 22:21:36 +0530

    [Ronit] Added : Contact book

---
+++ begginer_data_handling_projects/contact_book/main.py
@@ -0,0 +1,19 @@
+import json
+
+class ContactBook:
+    def __init__(self):
+        self.path = r"begginer_data_handling_projects\contact_book\contacts.json"
+
+    def add_contact(self):
+        
+        with open(self.path, 'a') as file:
+
+
+    def search_contact(self):
+        pass
+
+    def delete_contact(self):
+        pass
+
+    def view_all_contact(self):
+        pass
